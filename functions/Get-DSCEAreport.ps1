a925e417f6ab606c00511d24ac77a87fdf79260f
Robert M. Toups, Jr <robert.toups@gmail.com>
2020-09-15 22:57:20 -0400

    formatting for clarity

--- functions/Get-DSCEAreport.ps1
+++ functions/Get-DSCEAreport.ps1
@@ -172,7 +172,10 @@ This command returns non-compliant configuration file items detected, grouped by
     $Results |
       Select-Object -ExpandProperty Compliance |
       Where-Object { $_.PSComputerName -ne $null } |
-      Select-Object @{Name = "Computer"; Expression = { $_.PSComputerName } }, @{Name = "Compliant"; Expression = { $_.InDesiredState } } |
+      Select-Object -Property (
+        @{Name = "Computer"; Expression = { $_.PSComputerName } },
+        @{Name = "Compliant"; Expression = { $_.InDesiredState } }
+      ) |
       ConvertTo-HTML -Head $webstyle -body "<img src='C:\ProgramData\DSCEA\logo.png'/><br>", "<titlesection>DSC Configuration Report</titlesection><br>", "<datesection>Report last run on", $date, "</datesection><p>" |
       Out-File (Join-Path -Path $OutPath -ChildPath 'OverallComplianceReport.html')
     Get-ItemProperty (Join-Path -Path $OutPath -ChildPath 'OverallComplianceReport.html')
